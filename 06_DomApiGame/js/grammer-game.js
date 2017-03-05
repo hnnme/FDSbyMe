3ac3f41d175c9f6f1162bd1154f64adfda85a3e0
Ji Hyun Lee <jihyunlee@Jiui-MacBook-Pro.local>
2017-03-06 02:18:00 +0900

    grammer-game.js
    문제만드는함수추가 및 수정

--- 06_DomApiGame/js/grammer-game.js
+++ 06_DomApiGame/js/grammer-game.js
@@ -1,153 +1,82 @@
-// 고려해야 할점
-// 1) 만약에 2개이상의 선택을 했을시
-// 문제점
-// 1) 처음 선택시 안된다 ㅠ
-
-var append_btn, prepend_btn, after_btn, replace_btn,
-    txt_group,  replace_list;
-
-// 버튼
-append_btn    = query('.append-btn');
-prepend_btn   = query('.prepend-btn');
-after_btn     = query('.after-btn');
-replace_btn   = query('.replace-btn');
-
-// 그룹, 리스트
-txt_group     = query('.txt-group');
-replace_list  = query('.replace-list');
 
 
-var choose_target = null;
-var cnt = 0;
+  var append_btn, prepend_btn,   after_btn, before_btn,
+      txt_group,  question_list, btn_group,
+      choose_target, cnt,
+      ul, li, question_array, txt, keyword, random,
+      h1_el, h1_contents;
 
-// active 클래스로 만들기
-var chkList =function(e){
-  choose_target = this;
-  this.setAttribute('class', cnt%2 ? 'second' : 'active');
-  cnt = cnt%2 ? 0 : cnt+1 ;
-  replace_list.children.onclick = null;
-};
+  // 버튼
+  btn_group     = document.getElementsByTagName('button');
+  append_btn    = query('.append-btn');
+  prepend_btn   = query('.prepend-btn');
+  after_btn     = query('.after-btn');
+  before_btn    = query('.before-btn');
 
+  // 그룹, 리스트
+  txt_group     = query('.txt-group');
 
-// 리스트의 모든 li요소에 클릭이벤트 만들기
-for(var i=0; i<replace_list.children.length; i++){
-  replace_list.children[i].onclick = chkList;
-}
-/**
- * 유저가 선택한 답을 체크하는 함수
- */
-function checkAnswer(){
-  var answer = "";
-  for(var i=0; i<replace_list.children.length; i+=1){
-    answer += replace_list.children[i].innerText;
+  choose_target = null;
+  cnt           = 0;
+
+  // active 클래스로 만들기
+  var chkList =function(e){
+    choose_target = this;
+    this.setAttribute('class', cnt%2 ? 'second' : 'active');
+
+    if( cnt%2 ){
+      cnt = 0;
+      switchPosition();
+    } else {
+      cnt += 1;
+    }
+  };
+
+  // 랜덤으로 문제만들기
+  function makeQuiz(){
+    random  = Math.floor((Math.random() * 4) + 1);
+    random  = random == 4 ? 0 : random;
+    keyword = Object.keys(question)[random];
+
+    question_array = question[keyword].split('|');
+
+    ul = document.createElement('ul');
+    ul.setAttribute('class', 'question-list');
+    ul.setAttribute('role', 'tab-list');
+
+    for(; question_array.length > 0;){
+      random = Math.floor((Math.random() * ((question_array.length-1)+1)));
+      li  = document.createElement('li');
+      li.setAttribute('role', 'tab');
+      txt = question_array.splice(random-1, 1)[0];
+      txt = document.createTextNode(txt);
+      li.append(txt);
+      ul.appendChild(li);
+    }
+    txt_group.appendChild(ul);
   }
-  if( answerGame("replace") === answer ){
-    goodAnimation();
+  makeQuiz();
+  question_list = document.getElementsByTagName('ul').item(0);
+
+  // 리스트의 모든 li요소에 클릭이벤트
+  if(question_list){
+    for(var i =0; i<question_list.children.length; i++){
+      question_list.children[i].onclick = chkList;
+    }
+    for(var j=btn_group.length; j>0; j--){
+      btn_group[j-1].onclick = checkAnswer;
+    }
   }
-}
 
-/**
- * 문제의 답을 주는 함수
- * @param  {String} function_name - 함수 이름
- * @return {String}               - 답을 문자열로 반환
- */
-function answerGame(function_name){
-  var answer = "";
-  switch (function_name) {
-    case replace:
-      answer = "parentNode.replaceChild(newChild,oldChild);";
-      break;
-
-    case append:
-      answer = "";
-      break;
-
-    case prepend:
-      answer = "";
-      break;
-
-    case after:
-      answer = "";
-      break;
-
-    case replace:
-      answer = "";
-      break;
-
-    default: throw new Error("첫번째 인자값으로 함수명을 주셔야 합니다.");
-  }
-  return answer;
-}
-
-function removeClass(el_node){
-  if( el_node.nodeType !== 1 ){ throw new Error("첫번째 인자는 요소노드여야 합니다."); }
-  el_node.removeAttribute('class');
-}
-
-replace_btn.onclick = function(){
-  var changeItem = queryAll('.active', replace_list)[0];
-  var removeItem = queryAll('.second', replace_list)[0];
-  // 위치 바꾸기
-  change(changeItem, removeItem);
-  // 클래스속성을 없애서 배경색 없애기
-  removeClass(changeItem);
-  removeClass(removeItem);
-  checkAnswer();
-};
-
-
-
-
-
-append_btn.onclick = function(){
-  // 교체할 아이와
-  // 교체될 아이 찾기
-  // 클릭하면 교체될 아이 뒤에 교체할 아이가 들어감
-
-};
-
-prepend_btn.onclick = function(){
-
-};
-
-after_btn.onclick = function(){
-
-};
-
-// 해당 할일의 체크박스에 체크를 하면
-function goodAnimation(){
-    var success, modal, txt;
-    txt = "Good Job!";
-    success = document.createTextNode(txt);
-    modal = document.createElement('div');
-    modal.setAttribute('class','modal-good');
-    modal.appendChild(success);
-    document.body.appendChild(modal);
-}
-
-var h1_el, h1_contents;
+// h1요소 디자인을 위한 마크업변화
 h1_el = document.body.getElementsByTagName('h1').item(0);
 h1_contents = h1_el.innerText.split(" ");
-// [ heading 데코용 ]
-// h1_el.innerHTML = "<span>"+
-//                     h1_contents[0]+
-//                   "</span>"+
-//                   "<span>"+
-//                     h1_contents[1]+
-//                   "</span>"+
-//                   "<span>"+
-//                     h1_contents[2]+
-//                   "</span>";
-
-// 랜덤으로 문제 내기
-// 동적으로 리스트에 문제 넣기
-// 1) 랜덤함수를 이용해서 배열에서 문제들을 갖고 와서 li요소에 랜덤으로 뿌려주고
-// 동적으로 넣기
-// 문제를 갖고 있는 배열 만들기
-// 배열아이템을 분할하기
-var question ={
-  "append"  : "element.appendChild(aChild);",
-  "prepend" : "parentNode.insertBefore(newNode, referenceNode);",
-  "after"   : "",
-  "replace" : "parentNode.replaceChild(newChild,oldChild);",
-};
+h1_el.innerHTML = "<span>"+
+                    h1_contents[0]+
+                  "</span>"+
+                  "<span>"+
+                    h1_contents[1]+
+                  "</span>"+
+                  "<span>"+
+                    h1_contents[2]+
+                  "</span>";
